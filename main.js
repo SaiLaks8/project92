07b6b97a590dca47c8e0e5a3ff4e40687dfc5209
SaiLaks8 <83442017+SaiLaks8@users.noreply.github.com>
2022-05-11 16:27:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addUser(){
+    player1name = document.getElementById("player1_name").value;
+    player2name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1_name", player1name);
+    localStorage.setItem("player2_name", player2name);
+
+    window.location.replace("game_index.html");
+}
